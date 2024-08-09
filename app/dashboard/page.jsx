90a08bafe615bd27ae53c53176e6e9246439ac8a
Bishop Akintokun <bishopakinyinka@gmail.com>
2024-08-09 12:54:40 +0100

    booking card component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
 import { VscActivateBreakpoints } from "react-icons/vsc"
 import { FaHistory,FaUserCircle,FaRegCreditCard } from "react-icons/fa"
+import { BookingCard } from "@/Components/BookingCard"
 
 export default function Dashboard () {
     return (
-        <section className="lg:grid lg:grid-cols-2 gap-8 py-12 px-2 md:px-6 lg:px-12">
+        <section className="grid lg:grid-cols-2 gap-8 py-12 px-2 md:px-6 lg:px-12">
             <article className="grid grid-cols-2 gap-4">
                 <Link
                  href="/dashboard/book"
@@ -31,8 +32,14 @@ export default function Dashboard () {
                 <span className="text-xl text-white">Pay</span>
                 </Link>
             </article>
-            <aside className="bg-red-500">
+            <aside className="border border-gray-200 rounded-md p-4 md:p-6">
+                <blockquote className="border-b border-gray-200 pb-4">
+                    <p className="text-4xl">My Bookings</p>
+                </blockquote>
 
+                <div className="flex flex-col gap-4 md:gap-6">
+                    <BookingCard/>
+                </div>
             </aside>
         </section>
     )
